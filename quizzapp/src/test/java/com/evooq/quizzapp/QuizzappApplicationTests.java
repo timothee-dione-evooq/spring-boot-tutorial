b968191460a9a9cb06327ce2aaba8a1e2d3339ec
Timothée Dione <timothee.dione@evooq.ch>
2025-02-24 09:27:52 +0100

    First commit

---
+++ quizzapp/src/test/java/com/evooq/quizzapp/QuizzappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evooq.quizzapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizzappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
