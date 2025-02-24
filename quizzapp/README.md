dbe559a85464ea9354b9ef1ffd5a088ee1bc53b5
Timothée Dione <timothee.dione@evooq.ch>
2025-02-24 16:43:55 +0100

    endpoint /questions/allQuestions works

---
+++ quizzapp/README.md
@@ -0,0 +1,2 @@
+Run the app:
+``` ./gradlew bootRun ```
---
+++ quizzapp/src/main/java/com/evooq/quizzapp/QuestionController.java
@@ -0,0 +1,15 @@
+package com.evooq.quizzapp;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("question")
+public class QuestionController {
+
+    @GetMapping("allQuestions")
+    public String getAllQuestions() {
+        return "Hi, these are the questions";
+    }
+}
